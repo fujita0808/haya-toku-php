8313f38762b309a748dec3ee25d1fbd1f6f84be2
TWB_Fujita <fujita@twb.jp>
2026-03-12 16:11:39 +0900

    Update bootstrap.php

--- lib/bootstrap.php
+++ lib/bootstrap.php
@@ -9,6 +9,8 @@ const HAYA_TOKU_APP_NAME = 'HAYA-TOKU（🍊ver / PHP PoC）';
 const HAYA_TOKU_STORAGE_DIR = __DIR__ . '/../storage';
 
 require_once __DIR__ . '/functions.php';
+require_once __DIR__ . '/db.php';
 require_once __DIR__ . '/storage.php';
 require_once __DIR__ . '/coupon_logic.php';
 require_once __DIR__ . '/auth.php';
+
