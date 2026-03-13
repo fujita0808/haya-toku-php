960029239c27a9d2e1d740780ffb5ba99d252482
TWB_Fujita <fujita@twb.jp>
2026-03-13 15:15:08 +0900

    Add API headers for CORS and JSON response

---
+++ lib/api_headers.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type');
+header('Content-Type: application/json; charset=utf-8');
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit;
+}
